9d9f5a1bdacb0e6b7a6d51e56a61f57ef9b4e4b4
ignacy <ignacysiklucki@gmail.com>
2024-01-19 22:54:34 +0100

    fxml...

--- src/main/java/simulation/AppController.java
+++ src/main/java/simulation/AppController.java
@@ -1,9 +1,12 @@
 package simulation;//import simulation.Simulation;
 //import simulation.SimulationEngine;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
 import model.*;
 import javafx.fxml.FXML;
@@ -11,14 +14,22 @@ import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-//public class simulation.SimulationController implements MapChangeListener {
+//public class AppController implements MapChangeListener {
 public class AppController {
 //    private WorldMap map;
     @FXML private TextField animalCountField;
     @FXML private TextField grassCountField;
+    private final ExecutorService executorService;
+
+    public AppController() {
+        this.executorService = Executors.newFixedThreadPool(4);
+    }
 
     private boolean validAnimalCount(int value) {
         return 1<=value && value<=1000;
@@ -49,20 +60,30 @@ public class AppController {
 //        });
 //    }
 
-    public void runSimulation() {
-        ArrayList<Simulation> simulations = new ArrayList<>();
-//        List<MoveDirection> directions = OptionsParser.parse(textField.getText().split(" "));
+    public void runSimulation() throws IOException {
         List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
-
 //        WorldMap map = new GrassField(10);
 //        map.addObserver(new ConsoleMapDisplay());
-//        map.addObserver(this);
-//        setWorldMap(map);
 
-        simulations.add(new Simulation(positions));
+        SimulationController controller = new SimulationController();
+//        map.addObserver(controller);
+//        controller.setWorldMap(map);
 
-        SimulationEngine engine = new SimulationEngine(simulations);
-        engine.runAsync();
-//        engine.runAsyncInThreadPool();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setController(controller);
+        loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
+        BorderPane viewRoot = loader.load();
+
+        var scene = new Scene(viewRoot);
+        stage.setScene(scene);
+//        stage.setTitle("Simulation " + map.getId());
+        stage.minWidthProperty().bind(viewRoot.minWidthProperty());
+        stage.minHeightProperty().bind(viewRoot.minHeightProperty());
+        stage.show();
+
+//        Simulation simulation = new Simulation(positions, map);
+        Simulation simulation = new Simulation(positions);
+        executorService.submit(simulation);
     }
 }
